e8b48c02feebe9e025290df969dc0792984c2295
Nedzad <git@nedzadalibegovic.com>
2019-11-02 17:20:44 +0100

    Add vjezbe_2019_11_02

---
+++ pr3_nastava/vjezbe_2019_11_02/ILista.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vjezbe_2019_11_02 {
+    interface ILista<T> {
+        T[] Elementi { get; }
+
+        void Dodaj(T vrijednost);
+        void Brisi();
+        void Ispis<Z>(Z vrijednost);
+    }
+
+    class FITLista<T> : ILista<T> {
+        public T[] Elementi { get; private set; }
+        int pozicija = 0;
+
+        public FITLista() {
+            Elementi = new T[100];
+        }
+
+        public void Brisi() {
+            Elementi[--pozicija] = default;
+        }
+
+        public void Dodaj(T vrijednost) {
+            Elementi[pozicija++] = vrijednost;
+        }
+
+        public void Ispis<Z>(Z vrijednost) {
+            Console.WriteLine(typeof(Z).ToString());
+        }
+    }
+
+    class DotNetLista<T> : ILista<T> {
+        List<T> privatnaLista = new List<T>();
+        public T[] Elementi => privatnaLista.ToArray();
+
+        public void Brisi() {
+            privatnaLista.RemoveAt(0);
+        }
+
+        public void Dodaj(T vrijednost) {
+            privatnaLista.Add(vrijednost);
+        }
+
+        public void Ispis<Z>(Z vrijednost) {
+            Console.WriteLine(typeof(Z).ToString());
+        }
+    }
+}
---
+++ pr3_nastava/vjezbe_2019_11_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace vjezbe_2019_11_02 {
+    class Program {
+        static void Main(string[] args) {
+            ILista<int> lista = new DotNetLista<int>();
+
+            lista.Dodaj(4000);
+            lista.Dodaj(21);
+            lista.Brisi();
+
+            lista.Ispis("Test");
+        }
+    }
+}
