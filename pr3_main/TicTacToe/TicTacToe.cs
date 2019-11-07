62a6da0761f44e98bbe8150db39428aa1d358571
Nedzad <git@nedzadalibegovic.com>
2019-11-08 00:11:43 +0100

    Add WIP Tic-tac-toe implementation
    No game logic yet

---
+++ pr3_main/TicTacToe/TicTacToe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe {
+    public partial class TicTacToe : Form {
+        public int NumberOfMoves { get; set; }
+
+        public TicTacToe() {
+            InitializeComponent();
+            NumberOfMoves = 0;
+        }
+
+        private void markMove(object sender, EventArgs e) {
+            Button button = sender as Button;
+
+            // check that field is not marked
+            if (!string.IsNullOrEmpty(button.Text)) {
+                return;
+            }
+
+            if (NumberOfMoves % 2 == 0) {
+                button.Text = "X";
+            } else {
+                button.Text = "O";
+            }
+
+            NumberOfMoves++;
+        }
+    }
+}
